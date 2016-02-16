f6af4c36b3259c7c0a285afa700e4828de10c956
Gilles Coomans <gilles.coomans@gmail.com>
2016-02-16 01:04:09 +0100

    clean + rename transitions following html tag name convention

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ function slideUp(elem, ms, done) {
 	elem.style.maxHeight = '0';
 	return setTimeout(function() {
 		elem.style.opacity = '0';
-		if (done)
-			done();
+		if (done) done();
 	}, ms);
 }
 //___________________________________________
@@ -30,8 +29,7 @@ function slideLeft(elem, ms, done) {
 	elem.style.maxWidth = '0';
 	return setTimeout(function() {
 		elem.style.opacity = '0';
-		if (done)
-			done();
+		if (done) done();
 	}, ms);
 }
 //___________________________________________
@@ -44,8 +42,7 @@ function fadeOut(elem, ms, done) {
 	elem.style.opacity = '0';
 	return setTimeout(function() {
 		elem.style.opacity = '0';
-		if (done)
-			done();
+		if (done) done();
 	}, ms);
 }
 
@@ -53,33 +50,32 @@ function fadeOut(elem, ms, done) {
 
 function cssTransition(opt) {
 	return function(args) {
-		args = y.utils.copy(args);
-		args.ms = args.ms || 400;
-		args.ease = args.ease || 'ease-in-out';
-		args.timeout = null;
+		var timeout;
 		return this.once('mounted', function(container) {
 				var el = container.firstChild;
 				opt.initStyles(el, opt, args);
 				container.transitionIn = function(value) {
 					this.closing = false;
-					if (args.timeout)
-						clearTimeout(args.timeout);
+					if (timeout)
+						clearTimeout(timeout);
+					timeout = null;
 					opt.open(el, value !== undefined ? value : args.max);
 				};
 				container.transitionOut = function(done) {
 					this.closing = true;
-					if (args.timeout)
-						clearTimeout(args.timeout);
-					args.timeout = opt.close(el, args.ms, function() {
+					if (timeout)
+						clearTimeout(timeout);
+					timeout = opt.close(el, args.ms, function() {
 						container.closing = false;
 						done();
 					});
 				};
 			})
 			.on('mounted', function(container) {
-				if (args.timeout)
-					clearTimeout(args.timeout);
-				args.timeout = setTimeout(function() { container.transitionIn(); }, 15);
+				if (timeout)
+					clearTimeout(timeout);
+				// wait a bit before launching animation just after mount (else sometimes it doesn't start)
+				timeout = setTimeout(function() { container.transitionIn(); }, 15);
 			})
 			.dom(function(context, container) {
 				container.beforeUnmount(function(done) {
@@ -95,12 +91,12 @@ function initSlideStyles(el, opt, args) {
 	el.style[opt.prop] = args.max;
 	el.style.display = 'block';
 	el.style.opacity = 0;
-	el.style.transition = opt.transitionProp + ' ' + args.ms + 'ms ' + args.ease;
+	el.style.transition = opt.transitionProp + ' ' + (args.ms ||  400) + 'ms ' + (args.ease || 'ease-in-out');
 }
 
 y.toAPI('transition', {
-	//	template.use('transition:slideUp', { ms: 300, max: '100vh' })
-	slideUp: cssTransition({
+	// template.use('transition:slide-up', { ms: 300, max: '100vh' })
+	'slide-up': cssTransition({
 		prop: 'height',
 		transitionProp: 'max-height',
 		styleMax: 'maxHeight',
@@ -108,8 +104,8 @@ y.toAPI('transition', {
 		open: slideDown,
 		initStyles: initSlideStyles
 	}),
-	//	template.use('transition:slideLeft', { ms: 300, max: '100vw' })
-	slideLeft: cssTransition({
+	// template.use('transition:slide-left', { ms: 300, max: '100vw' })
+	'slide-left': cssTransition({
 		prop: 'width',
 		transitionProp: 'max-width',
 		styleMax: 'maxWidth',
@@ -124,7 +120,7 @@ y.toAPI('transition', {
 		initStyles: function(el, opt, args) {
 			el.style.display = 'block';
 			el.style.opacity = 0;
-			el.style.transition = 'opacity ' + args.ms + 'ms ' + args.ease + ' 0s';
+			el.style.transition = 'opacity ' + (args.ms ||  400) + 'ms ' + (args.ease || 'ease-in-out') + ' 0s';
 		}
 	})
 });
