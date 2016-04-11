19b6f3cfce4eb5353883d18e9a6587d720efce59
Gilles Coomans <gilles.coomans@gmail.com>
2016-04-11 13:29:52 +0200

    allow multiple children transitions

--- index.js
+++ index.js
@@ -1,93 +1,169 @@
 /**  
  * @author Gilles Coomans <gilles.coomans@gmail.com> 
  *
- * yamvish transitions manager linked to container mount and unmount mecanism (so only for dom output).
+ * yamvish transitions manager linked to container's (un)mounting mecanism (so only for dom output).
  * inspired from https://gist.github.com/ludder/4226288
  */
 var y = require('yamvish');
 
+// launch transitionIn on children
+function childrenIn(sub) {
+	if (sub.__yContainer__)
+		sub.remount();
+	else if (sub.transitionIn)
+		sub.transitionIn();
+}
+
+// launch transitionOut on children
+function childrenOut(sub) {
+	return new Promise(function(resolve) {
+		if (sub.__yContainer__)
+			sub.unmount(true, function() {
+				resolve(true);
+			});
+		else if (sub.transitionOut)
+			sub.transitionOut(resolve);
+		else
+			resolve(true);
+	});
+}
+
+// prepare container to manage children's transitions
+function prepareContainer(container) {
+	var timeout;
+	container.transitionChildren = [];
+	container.addWitness('transition');
+	container.transitionIn = function(value) {
+		this.closing = false;
+		if (timeout)
+			clearTimeout(timeout);
+		timeout = null;
+		this.transitionChildren.forEach(childrenIn);
+	};
+	container.transitionOut = function(done) {
+		this.closing = true;
+		if (timeout)
+			clearTimeout(timeout);
+		Promise.all(this.transitionChildren.map(childrenOut))
+			.then(function() {
+				if (!container.closing)
+					return;
+				container.closing = false;
+				if (done) done(true);
+			});
+	};
+	container.beforeUnmount(function(done) {
+		this.transitionOut(done);
+	});
+	container.on('mounted', function() {
+		if (timeout)
+			clearTimeout(timeout);
+		// wait a bit before launching animation just after mount (else sometimes it doesn't start)
+		timeout = setTimeout(function() { container.transitionIn(); }, 15);
+	});
+
+	// for destruction
+	container.binds = container.binds ||  [];
+	container.binds.push(function() {
+		container.transitionChildren = null;
+	});
+}
+
+// prepare dom node to in and out animation
+function prepareNode(el, args, opt) {
+	var timeout;
+	args.delay = args.delay || 5;
+	args.delay = Math.max(args.delay, 5);
+	if (opt.initStyles)
+		opt.initStyles(el, opt, args);
+	el.transitionIn = function(value) {
+		el.closing = false;
+		if (timeout)
+			clearTimeout(timeout);
+		timeout = setTimeout(function() {
+			opt.open(el, value !== undefined ? value : args.max);
+		}, args.delay);
+	};
+	el.transitionOut = function(done) {
+		el.closing = true;
+		if (timeout)
+			clearTimeout(timeout);
+		timeout = opt.close(el, args.ms || 400, function() {
+			el.closing = false;
+			done(true);
+		});
+	};
+}
+
+// bind container mounting and transitions to parent's mounting mecanism
+function bindTransitionToParent(container) {
+	if (!container.transitionChildren)
+		prepareContainer(container);
+	if (container.parent) {
+		if (!container.parent.transitionChildren)
+			prepareContainer(container.parent);
+		container.parent.transitionChildren.push(container);
+	} else
+		throw new Error('could not bind transition to parent container : no parent found');
+	container.parentBinded = true;
+}
+
+// produce specific transition template handler
 function transition(opt) {
 	return function(args) {
 		args = args || {};
-		args.delay = args.delay || 15;
-		args.delay = Math.max(args.delay, 15);
-		var timeout;
-		return this.once('mounted', function(context, container) {
-				var el = container.firstChild;
-				if (opt.initStyles)
-					opt.initStyles(el, opt, args);
-				if (args.bindToParent)
-					if (container.parent) {
-						container.addWitness('subtransitioned child');
-						container.parent._subtransitioned = container.parent._subtransitioned || [];
-						container.parent._subtransitioned.push(container);
-					} else
-						throw new Error('could not bind transition to parent container : no parent found');
-				container.transitionIn = function(value) {
-					this.closing = false;
-					if (timeout)
-						clearTimeout(timeout);
-					timeout = null;
-					opt.open(el, value !== undefined ? value : args.max);
-					if (this._subtransitioned) {
-						this._subtransitioned.forEach(function(sub) {
-							sub.remount();
+		return this.dom(function(context, node, a, container) {
+			if (!container.transitionChildren)
+				prepareContainer(container);
+			if (args.bindToParent && !container.parentBinded)
+				bindTransitionToParent(container);
+			if (node.__yContainer__) {
+				if (node === container) {
+					if (!container.mounted)
+						node.once('mount', function() {
+							prepareNode(node.firstChild, args, opt);
+							container.transitionChildren.push(node.firstChild);
 						});
+					else {
+						prepareNode(node.firstChild, args, opt);
+						container.transitionChildren.push(node.firstChild);
 					}
-				};
-				if (opt.close) {
-					container.transitionOut = function(done) {
-						this.closing = true;
-						if (timeout)
-							clearTimeout(timeout);
-						var promises = [],
-							resolver;
-						promises.push(new Promise(function(resolve) {
-							resolver = resolve;
-						}));
-						timeout = opt.close(el, args.ms || 400, function() {
-							resolver(true);
-						});
-						if (this._subtransitioned)
-							this._subtransitioned.forEach(function(sub) {
-								var p = new Promise(function(resolve) {
-									sub.unmount(true, function() {
-										resolve(true);
-									});
-								});
-								promises.push(p);
-							});
-						Promise.all(promises).then(function() {
-							if (!container.closing)
-								return;
-							container.closing = false;
-							done();
-						});
-					};
-					container.beforeUnmount(function(done) {
-						this.transitionOut(done);
-					});
-				}
-			})
-			.on('mounted', function(context, container) {
-				if (timeout)
-					clearTimeout(timeout);
-				// wait a bit before launching animation just after mount (else sometimes it doesn't start)
-				timeout = setTimeout(function() { container.transitionIn(); }, args.delay);
-			});
-	}
-};
+				} else
+					container.transitionChildren.push(node);
+			} else {
+				prepareNode(node, args, opt);
+				container.transitionChildren.push(node);
+			}
+			if (container.mounted)
+				node.transitionIn();
+		});
+	};
+}
+
+//___________________________________________________ transitions def
 
 function initSlideStyles(el, opt, args) {
+	// el.style.display = 'block';
 	el.style.overflow = 'hidden';
 	el.style[opt.styleMax] = 0;
 	el.style[opt.prop] = args.max;
-	el.style.display = 'block';
 	el.style.opacity = 0;
 	el.style.transition = opt.transitionProp + ' ' + (args.ms ||  400) + 'ms ' + (args.ease || 'ease-in-out');
 }
 
-y.toAPI('transition', {
+function initFadeStyles(el, opt, args) {
+	// el.style.display = 'block';
+	el.style.opacity = 0;
+	el.style.transition = 'opacity ' + (args.ms ||  400) + 'ms ' + (args.ease || 'ease-in-out') + ' 0s';
+}
+
+module.exports = y.toAPI('transition', {
+	bindTransitionToParent: function() {
+		return this.dom(function(context, node, args, container) {
+			if (!container.parentBinded)
+				bindTransitionToParent(container);
+		});
+	},
 	// usage : template.use('transition:slide-up', { ms: 300, max: '100vh' })
 	'slide-up': transition({
 		prop: 'height',
@@ -136,11 +212,7 @@ y.toAPI('transition', {
 		open: function(elem) {
 			elem.style.opacity = '1';
 		},
-		initStyles: function(el, opt, args) {
-			el.style.display = 'block';
-			el.style.opacity = 0;
-			el.style.transition = 'opacity ' + (args.ms ||  400) + 'ms ' + (args.ease || 'ease-in-out') + ' 0s';
-		}
+		initStyles: initFadeStyles
 	}),
 	// template.use('transition:fade-in', { ms: 300 })
 	// no fade-out.
@@ -148,20 +220,6 @@ y.toAPI('transition', {
 		open: function(elem) {
 			elem.style.opacity = '1';
 		},
-		initStyles: function(el, opt, args) {
-			el.style.display = 'block';
-			el.style.opacity = 0;
-			el.style.transition = 'opacity ' + (args.ms ||  400) + 'ms ' + (args.ease || 'ease-in-out') + ' 0s';
-		}
-	}),
-	// template.use('transition:cut')
-	// useful for cascading transition management.
-	cut: transition({
-		open: function(elem) {},
-		close: function(elem, ms, done) {
-			return setTimeout(function() {
-				if (done) done();
-			}, ms);
-		}
+		initStyles: initFadeStyles
 	})
 });
